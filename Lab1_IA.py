a666ea9cc51c0acb595c88b02c54e61fa1633b9a
bjperengueza <brandon.jperen@gmail.com>
2022-05-24 01:09:35 -0500

    Verificación_temp_otros depart

--- Lab1_IA.py
+++ Lab1_IA.py
@@ -11,7 +11,8 @@ def cam_termica():
     depart_ingreso = input("Ingrese el departamento que desee utilizar la camara termica: ")
     #ingrese el registro de la temperatura
     temp_ingreso = input("La temperatura del departamento es: ")
-    nueva_temp_ingreso= input("La temperatura de otro departamentos es:")
+    temp_ingreso1= input("La temperatura de otro departamentos es:")
+    temp_ingreso2= input("La temperatura del siguiente departamentos es:")
     #objetivo a cumplir
     print("Se busca llegar a la temperatura: "+ str(objetivo))
 
@@ -23,14 +24,39 @@ def cam_termica():
         if temp_ingreso == '1':
             #mensaje de alerta
             print("Se ha registrado una temperatura alta dentro del departamento 1.")
+            print("Activar el sistema de enfriamiento")
             #activar el sistema de enfriamiento
             objetivo['departamento1'] = '0'
             #incremento del costo por el funcionamiento
             costo += 1
             #mensaje de la solución y aumento del costo
-            print("Temperatura del departamento establecida en baja.")
+            print("Temperatura del departamento 1 establecida en baja.")
             print("Costo actual: "+str(costo))
 
+            if temp_ingreso1 == '1':
+                print("El departamento 2 registra una temperatura alta")
+                print("Activar el sistema de enfriamiento")
+                objetivo['departamento2'] = '0'
+                costo +=1 
+                print("Temperatura del departamento 2 establecida en baja.")
+                print("Costo actual: "+str(costo))
+            else:
+                print("El departamento 2 se encuentra con una temperatura baja")
+                print("No se efectuo ninguna acción. Costo actual" + str(costo))
             
+            if temp_ingreso2 == '1':
+                print("El departamento 3 registra una temperatura alta")
+                print("Activar el sistema de enfriamiento")
+                objetivo['departamento3'] = '0'
+                costo +=1 
+                print("Temperatura del departamento 2 establecida en baja.")
+                print("Costo actual: "+str(costo))
+            else:
+                print("El departamento 3 se encuentra con una temperatura baja")
+                print("No se efectuo ninguna acción. Costo actual" + str(costo))
+
+        
+
+
             
 
