3a2ca1c00ff5acb9788eefee2795a686dfcb923b
bjperengueza <brandon.jperen@gmail.com>
2022-05-23 23:41:46 -0500

    Definicion_metas_costo

--- Lab1_IA.py
+++ Lab1_IA.py
@@ -1,3 +1,7 @@
 #Instrucciones
 #ingresar el departamento (ejem: departamento1)
+#ingresar el estado del departamento (temperatura alta = 1 o temperatura baja = 0)
 def cam_termica():
+    #definir las metas que se buscan alcanzar y costo inicial
+    objetivo = {'departamento1':'0', 'departamento2':'0', 'departamento3':'0', }
+    costo = 0
