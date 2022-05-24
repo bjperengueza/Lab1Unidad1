9b09a74986b323d30c1c31953add511d6c47fbf9
bjperengueza <brandon.jperen@gmail.com>
2022-05-24 00:14:28 -0500

    Ingreso de datos

--- Lab1_IA.py
+++ Lab1_IA.py
@@ -5,3 +5,14 @@ def cam_termica():
     #definir las metas que se buscan alcanzar y costo inicial
     objetivo = {'departamento1':'0', 'departamento2':'0', 'departamento3':'0', }
     costo = 0
+
+    #ingreso de datos por el usuario
+    #Ingreso del departamento donde se va a utilizar la camara 
+    depart_ingreso = input("Ingrese el departamento que desee utilizar la camara termica: ")
+    #ingrese el registro de la temperatura
+    temp_ingreso = input("La temperatura del departamento es: ")
+    nueva_temp_ingreso= input("La temperatura de otro departamentos es:")
+    #objetivo a cumplir
+    print("Se busca llegar a la temperatura: "+ str(objetivo))
+
+    
